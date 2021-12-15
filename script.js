e66f940f2de877975cc8cfe135800b89a673ff7d
moulsiri <maulsiri2301@gmail.com>
2021-12-15 18:23:40 +0530

    need to chenge the approach

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let content=document.querySelector("#content");
-let inptModal=document.querySelector("#category");
-let cinpt=document.querySelector("#inpt input");
-let ctgList=document.querySelector("#cclip");
 let flag=1;
-let showBtn=document.querySelector("#showClip");
-let checkCtg=document.querySelector("#show");
+let content=document.querySelector("#content");
+let cinpt=document.querySelector("#inpt input");
+let ctgList=document.querySelector("#ctgList");
 
 //chalo mouseover bhi lag gya
 // let overlay=document.querySelectorAll(".ovly");
@@ -29,12 +26,12 @@ content.addEventListener("mouseout",function(e){
 // button to show all categories
 document.querySelector("#cbtn").addEventListener("click",function(){
     if(flag===1){
-        showBtn.style.width="30%";
+        document.querySelector("#showClip").style.width="30%";
         document.querySelector("#show").style.display="flex"
         flag=0;
     }
     else{
-        showBtn.style.width="0%";
+        document.querySelector("#showClip").style.width="0%";
         document.querySelector("#show").style.display="none"
         flag=1;
 
@@ -51,39 +48,45 @@ document.querySelector("#srch").addEventListener("mouseout",function(){
     this.style.boxShadow="none";
  })
 
+
+
 // Array of data to store whole data 
-let data=[
-    {cid:0, img:"https://images.unsplash.com/photo-1632924192284-9163f3d88a16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Gift Packages",saved:false},
-    {cid:1, img:"https://images.unsplash.com/photo-1542318332-034f3136b62f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",name:"Flowers n Beauty",saved:false},
-    {cid:2, img:"https://images.unsplash.com/photo-1607092683781-a3146e0a1d49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Chocolate Chip Cookies",saved:false},
-    {cid:3, img:"https://images.unsplash.com/photo-1483201811930-882b8be091e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"The best way to start",saved:false},
-    {cid:4, img:"https://images.unsplash.com/photo-1608473430561-df11ffde8b21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",name:"Puppy in the house",saved:false},
-    {cid:5, img:"https://images.unsplash.com/photo-1565992441121-4367c2967103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",name:"Athletics | Snow",saved:false},
-    {cid:6, img:"https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=441&q=80",name:"Cycling is Love",saved:false},
-    {cid:7, img:"https://images.unsplash.com/photo-1561001070-ce11eea271b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"Kuch khana bhi hai",saved:false},
-    {cid:8, img:"https://images.unsplash.com/photo-1579710759212-d869a36773ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Winter Peace",saved:false},
-    {cid:9, img:"https://images.unsplash.com/photo-1622277695245-c1694a6c0b36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Mount Everest",saved:false},
-    {cid:10, img:"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Laughter | Girl",saved:false},
-    {cid:11, img:"https://images.unsplash.com/photo-1591153952470-8eede4a8f00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",name:"Tired af",saved:false},
-    {cid:12, img:"https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Mediterranean Salad",saved:false},
-    {cid:13, img:"https://images.unsplash.com/photo-1508473161895-5dabcc2f59cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"View | Girl with cape",saved:false},
-    {cid:14, img:"https://images.unsplash.com/photo-1624589010366-365b61efb41b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Beautiful site | Mountain",saved:false}
+let baseData=[
+    { img:"https://images.unsplash.com/photo-1632924192284-9163f3d88a16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Gift Packages",saved:false},
+    { img:"https://images.unsplash.com/photo-1542318332-034f3136b62f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",name:"Flowers n Beauty",saved:false},
+    { img:"https://images.unsplash.com/photo-1607092683781-a3146e0a1d49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Chocolate Chip Cookies",saved:false},
+    { img:"https://images.unsplash.com/photo-1608473430561-df11ffde8b21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",name:"Puppy in the house",saved:false},
+    { img:"https://images.unsplash.com/photo-1483201811930-882b8be091e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"The best way to start",saved:false},
+    { img:"https://images.unsplash.com/photo-1565992441121-4367c2967103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",name:"Athletics | Snow",saved:false},
+    { img:"https://images.unsplash.com/photo-1533107862482-0e6974b06ec4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=441&q=80",name:"Cycling is Love",saved:false},
+    { img:"https://images.unsplash.com/photo-1579710759212-d869a36773ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Winter Peace",saved:false},
+    { img:"https://images.unsplash.com/photo-1622277695245-c1694a6c0b36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Mount Everest",saved:false},
+    { img:"https://images.unsplash.com/photo-1561001070-ce11eea271b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"Kuch khana bhi hai",saved:false},
+    { img:"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Laughter | Girl",saved:false},
+    { img:"https://images.unsplash.com/photo-1591153952470-8eede4a8f00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",name:"Tired af",saved:false},
+    { img:"https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",name:"Mediterranean Salad",saved:false},
+    { img:"https://images.unsplash.com/photo-1508473161895-5dabcc2f59cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"View | Girl with cape",saved:false},
+    { img:"https://images.unsplash.com/photo-1624589010366-365b61efb41b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Beautiful site | Mountain",saved:false},
+    { img:"https://images.unsplash.com/photo-1639501838991-215ffc3cf280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",name:"Beautiful candel | Xmas",saved:false},
+    { img:"https://images.unsplash.com/photo-1572448862527-d3c904757de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",name:"Burger | fast food",saved:false},
+    { img:"https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=480&q=80",name:"Honey | Pancakes",saved:false},
+    { img:"https://images.unsplash.com/photo-1504439904031-93ded9f93e4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",name:"Model | Pose",saved:false},
+    { img:"https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80",name:"Architecture",saved:false},
 ]
 // Array to store info about the categories 
-let ctgData=[
-    {name:"Favorite"},
-]
-
-
-
+let ctgData={
+    Favorite:[],
+    Food:[],
+    Sports:[]
+}
 //Setting data to the local storage
 let local=window.localStorage;
-setLocal=(arr,ctg)=>{
-    local.setItem(ctg,JSON.stringify(arr));
+setLocal=(arr,key)=>{
+    local.setItem(key,JSON.stringify(arr));
 }
 //retrieve the data
-retrieve=(ctg)=>{
-    return JSON.parse(local.getItem(ctg));
+retrieve=(key)=>{
+    return JSON.parse(local.getItem(key));
 }
 // function to show all the image cards
 function showCards(arr){
@@ -111,231 +114,38 @@ function showCards(arr){
     })
     content.innerHTML=t;
 }
-//function of delete the category from the array
-delCtg=(index)=>{
-    let arr=retrieve("category");
-    arr.splice(index,1);
-    console.log(arr)
-    return arr;
-    }
-    //refresh the main data after the category is removed 
-    delCtgFromData=(ctg)=>{
-        let arr=retrieve("mainData");
-        arr.forEach(function(e,i){
-            if(e.ctg===ctg){
-                e.saved=false;
-                delete e.ctg;
-            }
-        })
-        console.log(arr);
-        return arr;
-    }
-
-// Pinning the images 
-// #main event
-content.addEventListener("click",function(e){
-    let tempArr=retrieve("mainData");
-    // let idd=(e.target.id).split('');
-    // let index=tempArr[idd[0]][idd[1]];
-    let index=e.target.id;   
-    if(e.target.classList[0]==="sbtn"){
-        tempArr[index].saved=!tempArr[index].saved
-        setLocal(tempArr,"mainData");
-        tempArr=retrieve("mainData");
-        showCards(tempArr);
-
-        inptModal.addEventListener("click",function(dets){
-            //If User wants to close the window
-            // console.log(dets.target);
-             if(dets.target.id==="cls"){
-                            tempArr[index].saved=!tempArr[index].saved
-        
-                            setLocal(tempArr,"mainData");
-                            tempArr=retrieve("mainData");
-                            inptModal.style.display="none";
-                            showCards(tempArr);
-                        }
-        })
-        if(tempArr[index].saved){
-            inptModal.style.display="initial";
-            cinpt.disabled=false;
-            cinpt.focus();
-            ctgList.addEventListener("click",function(d){
-                // let ctg=clips[d.target.id].querySelector("a").textContent;
-                let tempCtg=retrieve("category");
-                showCtg(tempCtg);
-                // console.log(d.target.id);
-                tempArr[index].ctg=tempCtg[d.target.id].name;
-                setLocal(tempArr,"mainData");
-
-                tempArr=retrieve("mainData");
-                showCards(tempArr);
-                inptModal.style.display="none";
-            })
-
-            // showCtg("mainData");
-        }
-        else{
-            delete tempArr[index].ctg;
-            setLocal(tempArr,"mainData");
-            tempArr=retrieve("mainData");
-            showCards(tempArr)
-        }
-    }
-    if(e.target.classList[2]==="sctg"){
-        console.log("hello");
-        inptModal.style.display="initial";
-        cinpt.disabled=true;
-        inptModal.addEventListener("click",function(dets){
-            if(dets.target.id==="cls"){
-                inptModal.style.display="none";
-            }
-
-        })
-//Event to rmove the any Existing Categories
-        ctgList.addEventListener("click",function(e){
-            // console.log(e.target.classList);
-            if(e.target.classList[2]==="close"){
-                let tCtg=retrieve("category");
-                // console.log(tCtg[e.target.id].name);
-                setLocal(delCtgFromData(tCtg[e.target.id].name),"mainData");
-                // let arrrr=retrieve("mainData");
-                // console.log(arrrr);
-                showCards(retrieve("mainData"));
-                setLocal(delCtg(e.target.id),"category");
-                showCtg(retrieve("category"));
-                showCheck(retrieve("category"));
-                cinpt.focus();
-                inptModal.style.display="none";
-                showCards(retrieve("mainData"));
-        
-            }
-        })
-
-    }
-
-})
-
-//Showing the data category wise
-filter=(ctg)=>{
-    let arr=retrieve("mainData");
-    // console.log(arr);
-    let arr2=[];
-    arr.forEach(function(elm,i){
-            if(elm.ctg===ctg){
-               arr2.push(elm);
-            }
-        
-        
-        })
-    // console.log(arr2);
-    return arr2;
-}
-checkCtg.addEventListener("change",function(d){
-    if(d.target.className==="cursor"){
-        if(this.querySelectorAll("input")[d.target.id].checked){
-            // console.log("hello")
-            showCards(filter(document.querySelectorAll("label")[d.target.id].textContent));
-            content.style.flexDirection="row";
-        }
-        else{
-            content.style.flexDirection="column";
-            let dt=retrieve("mainData")
-            showCards(dt);
-        }
-    }
-})
-
-
-
-
-// Categorising the data related to the user INput 
-// getting input value
-function getCtg(){
-    return cinpt.value.trim();
-}
-function showCtg(ctgArr){
-    // let d1=document.createElement("div");
-    // d1.classList.add("cctg");
-    // let a=document.createElement("a");
-    // a.textContent=ctg;
-    // d1.append(a);
-    // ctgList.append(d1);
+function showCtg(ctg){
     let t="";
-    ctgArr.forEach(function(e,i){
-        t+=`<div class="cctg" id="${i}">
-        <i class="ri-close-circle-fill cursor close" id="${i}"></i>
-        <a href="#" id="${i}">${e.name}</a>
+    for(let i in ctg){
+        t+=` <div class="cctg">
+        <a download href="./github.png">${i}</a>
+        <i class="ri-close-circle-fill"></i>
     </div>`
-    })
-    ctgList.innerHTML=t;
-
-    
-}
-//to show categories as checkbox
-function showCheck(ctgArr){
-    let t="";
-    ctgArr.forEach(function(e,i){
-        t+=`
-        <input type="checkbox" id="${i}" class="cursor">
-        <label for="${i}" class="cursor">${e.name}</label>
-        `
-    })
-    checkCtg.innerHTML=t;
-}
-
-// In this way the user can make there own category
-cinpt.addEventListener("keydown",function(e){
-    if(e.keyCode===13){
-        let tempCtg=retrieve("category");
-        tempCtg.push({name:getCtg()});
-        setLocal(tempCtg,"category");
-        showCtg(retrieve("category"));
-        showCheck(retrieve("category"));
-        cinpt.value="";
-        cinpt.blur();
     }
-    
+    // console.log(t);
+    ctgList.innerHTML=t;
+}
+
+//Main event
+content.addEventListener("click",function(dets){
+    console.log(dets);
+    if(dets.target.classList[0]==="sbtn"){
+        console.log("hello");
+    }
 })
-
-
 
 
 
 
 //Initial work
 if(local.getItem("mainData")===null){
-    setLocal(data,"mainData");
+    setLocal(baseData,"mainData");
 }
 if(local.getItem("category")===null){
     setLocal(ctgData,"category");
 }
-// setLocal(data,"mainData");
+// setLocal(baseData,"mainData");
 // setLocal(ctgData,"category");
+showCards(retrieve("mainData"));
+showCtg(retrieve("category"));
 
-
-let main=retrieve("mainData");
-let ctgArr=retrieve("category");
-showCtg(ctgArr);
-showCheck(ctgArr);
-showCards(main);
-
-
-// create array (not used yet)
-function getElement(elem,ctg){
-    let arr2=retrieve(ctg);
-    if(arr2===null){
-        let ar=[];
-        ar.push(elem);
-        setLocal(ar,ctg);
-        return ar;
-        
-    }
-    else{
-        arr2.push(elem);
-    }
-    console.log(arr2);
-    arr2.push(elem);
-    return arr2;
-
-}
